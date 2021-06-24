75aa02c2aabfde0dac95ecf6abb6a427e0fd6a84
olehandros <olehandros@rambler.ru>
2021-06-24 19:06:09 +0300

    questionnaire

--- front/src/App.js
+++ front/src/App.js
@@ -1,8 +1,16 @@
+import { Divider } from '@material-ui/core';
 import './App.css';
+import Questionnaire from './components/Questionnaire/Questionnaire';
+import Newtext from './components/newtext/Newtext'
 
 function App() {
   return (
+    <>
     <h1>CINDER</h1>
+    {/* <Newtext/> */}
+
+    <Questionnaire/>
+    </>
   );
 }
 
