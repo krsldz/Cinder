5a487260ae171b3cd788e2f4350762d89658e8cb
olehandros <olehandros@rambler.ru>
2021-06-28 15:47:36 +0300

    add in Back: three filters

--- back/routers/compilation.js
+++ back/routers/compilation.js
@@ -1,11 +1,27 @@
 const { Router } = require('express');
 const bcrypt = require('bcrypt');
 const User = require('../models/user');
+const Films = require('../models/film')
 
 const router = Router();
 
-router.post('/compilation',(req,res)=>{
-  console.log(req.body);
+router.post('/compilation', async(req,res)=>{
+  
+  const films = await Films.find();
+  
+//let {jenre, withWhom, mood} = req.body;
+ let firstFilter = films.filter((el)=> el.withWom.includes(req.body.withWhom) )
+  // console.log(firstFilter);
+  let secondFilter = firstFilter.filter((el) => el.mood.includes(req.body.mood))
+  // console.log(secondFilter);
+  const thirdFilter =(req, filtrus) =>{
+    for (let i=0; i<req.length; i++){
+     let third = filtrus.filter((el)=> el.genre.includes(req[i]));
+     return third;
+    }
+  }
+console.log(thirdFilter(req.body.jenre, secondFilter));
+ 
 })
 
 
