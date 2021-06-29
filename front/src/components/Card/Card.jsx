a5545cca2dda42c367eece64d5162bb4c4ab7ebc
Danya <danya.culakov@yandex.ru>
2021-06-29 18:19:20 +0300

    rel10 new Profile new

--- front/src/components/Card/Card.jsx
+++ front/src/components/Card/Card.jsx
@@ -57,7 +57,7 @@ const onCardLeftScreen = (myIdentifier) => {
 
   return (
   <>
-    <TinderCard onSwipe={onSwipe}  preventSwipe={['right', 'left']}> aaaaaaa 
+    <TinderCard onSwipe={onSwipe}  preventSwipe={['right', 'left']}>
     <div className="card">
       <div className="dws-wrapper">
         <a>
