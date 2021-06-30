49d2b56d41fef34dd156e000906ba81002e9880f
MariaR <8insouciance@gmail.com>
2021-06-30 15:29:37 +0300

    Perezasev baze

--- front/src/components/SrollBar/ScrollBar.jsx
+++ front/src/components/SrollBar/ScrollBar.jsx
@@ -1,6 +1,37 @@
 import Zak from "../../images/Zak.jpeg";
+import React, { useEffect, useState } from "react";
 
 export default function ScrollBar() {
+
+  const [infoAboutFilm, setInfoAboutFilm] = useState({});
+  const [infoAboutFilm2, setInfoAboutFilm2] = useState({});
+
+  const movieInfo = () => {
+    fetch(
+      `https://api.kinopoisk.cloud/movies/1108577/token/de2e011c57a2199d24402bc642e73394`
+    )
+      .then((res) => res.json())
+      .then((data) => setInfoAboutFilm(data));
+  }
+ 
+  useEffect(() => {
+  movieInfo()
+ }, [])
+
+ const movieInfo2 = () => {
+  fetch(
+    `https://api.kinopoisk.cloud/movies/558/token/de2e011c57a2199d24402bc642e73394`
+  )
+    .then((res) => res.json())
+    .then((data) => setInfoAboutFilm2(data));
+}
+
+useEffect(() => {
+movieInfo2()
+}, [])
+
+ 
+
   return (
     <div
       className="uk-position-relative uk-visible-toggle uk-light"
@@ -9,18 +40,18 @@ export default function ScrollBar() {
     >
       <ul className="uk-slider-items  uk-child-width-1-4@m ">
         <li className="uk-transition-toggle" tabindex="0">
-          <img src={Zak} alt="" />
+          <img src={infoAboutFilm.poster} alt="" />
           <div className="uk-position-center uk-panel">
             <div className="uk-h1 uk-transition-slide-bottom-small textScroll">
-              1
+            {infoAboutFilm.rating_kinopoisk}
             </div>
           </div>
         </li>
         <li className="uk-transition-toggle" tabindex="0">
-          <img src={Zak} alt="" />
+          <img src={infoAboutFilm2.poster} alt="" />
           <div className="uk-position-center uk-panel">
             <div className="uk-h1 uk-transition-slide-bottom-small textScroll">
-              1
+            {infoAboutFilm2.rating_kinopoisk}
             </div>
           </div>
         </li>
