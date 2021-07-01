83fdb1abebb118297d56dda78e1775dc20277ac0
olehandros <olehandros@rambler.ru>
2021-07-01 16:50:07 +0300

    saas

--- back/routers/userLikeFilm.js
+++ back/routers/userLikeFilm.js
@@ -2,6 +2,7 @@ const { Router } = require('express');
 const router = Router();
 const Like = require('../models/like');
 const SuperLike = require('../models/superlike');
+const Viewed = require('../models/viewed');
 
 
 router.get('/superlikedFilm', async (req, res)=>{
@@ -46,6 +47,7 @@ router.post('/superlikedFilm', async(req,res)=>{
 router.get('/likedFilm', async (req, res)=>{
   const result = await Like.find({id:req.session.user.id});
   
+  
    res.json(result);
 
   
@@ -65,19 +67,38 @@ router.post('/likedFilm', async(req,res)=>{
       movie: newArr,
 
     })
-    
-
-  
-  
   res.json(result);
-  
-
-  
-  
-  
-
-
-  
 })
+router.post('/view/superlike',async(req,res)=>{
+  await SuperLike.findByIdAndDelete({_id:req.body._id});
+  await Viewed.create({
+    id: req.body.id,
+    movie: req.body.movie,
+  })
+
+  res.json(req.body);
+
+})
+router.post('/view/like',async(req,res)=>{
+  await Like.findByIdAndDelete({_id:req.body._id});
+  await SuperLike.create({
+    id: req.body.id,
+    movie: req.body.movie,
+  })
+
+  res.json(req.body);
+
+})
+
+
+router.get('/view', async(req,res)=>{
+  let result = await Viewed.find();
+  if(result){
+
+    res.json(result);
+  }
+  else res.json('')
+})
+
 
 module.exports = router;
