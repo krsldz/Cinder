e7f046d46c7abb90735242ad6d460a8c40f50b71
Ivan Paponin <eldkirk@gmail.com>
2021-07-01 12:49:39 +0300

    before pull from origin

--- front/src/components/Profile/Profile.jsx
+++ front/src/components/Profile/Profile.jsx
@@ -57,7 +57,6 @@ function Profile() {
   const user = useSelector((state) => state.user);
 
   useEffect(() => {
-    console.log('useEffect');
     if (user === null) {
       dispatch(getUserFromServer());
     }
